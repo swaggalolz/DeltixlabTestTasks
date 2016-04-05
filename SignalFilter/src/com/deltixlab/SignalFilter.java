36bf8336b44a65d378bb99840a38b87b8e4cc620
swaggalolz <rosmen1@tut.by>
2016-04-05 15:38:37 -0300

    replaced with ArrayBlockingQueu on a LinkedList

---
+++ SignalFilter/src/com/deltixlab/SignalFilter.java
@@ -0,0 +1,56 @@
+package com.deltixlab;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SignalFilter implements Filter {
+
+	public SignalFilter(int N, long limitTimeInterval) {
+		this.N = N;
+		this.limitTimeInterval = limitTimeInterval * 1000;
+		this.signalGenerations = new LinkedList<Long>();
+
+	}
+
+	private int N;
+	private long limitTimeInterval;
+	private Queue<Long> signalGenerations;
+
+	
+	@Override
+	public synchronized boolean isSignalAllowed() {
+
+		if (this.signalGenerations.size() < this.N - 1) {
+			signalGenerations.offer(System.currentTimeMillis());
+			return true;
+		} else {
+			long timeNow = System.currentTimeMillis();
+			long currentTimeInterval = timeNow - this.signalGenerations.peek();
+			if (currentTimeInterval > this.limitTimeInterval) {
+				this.signalGenerations.poll();
+				this.signalGenerations.offer(timeNow);
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+	}
+
+	public int getN() {
+		return N;
+	}
+
+	public void setN(int n) {
+		N = n;
+	}
+
+	public long getLimitTimeInterval() {
+		return limitTimeInterval;
+	}
+
+	public void setLimitTimeInterval(long limitTimeInterval) {
+		this.limitTimeInterval = limitTimeInterval;
+	}
+
+}

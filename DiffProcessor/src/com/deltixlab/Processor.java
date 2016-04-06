f8071b3998f30ff97c0fe99fe689e3667fb51a1c
swaggalolz <rosmen1@tut.by>
2016-04-06 08:39:35 -0300

    add some checks

--- DiffProcessor/src/com/deltixlab/Processor.java
+++ DiffProcessor/src/com/deltixlab/Processor.java
@@ -8,23 +8,27 @@ import com.deltixlab.SortedLimitedList.Entry;
  * Created by VavilauA on 6/19/2015.
  */
 public class Processor {
-    public Processor(long limit) {
-        this.limit = limit;
-    }
+	public Processor(long limit) {
+		this.limit = limit;
+	}
 
-    private long limit;
-        
-    public void doProcess(SortedLimitedList<Double> mustBeEqualTo, SortedLimitedList<Double> expectedOutput) {
-    	mustBeEqualTo.clear();
-    	Entry<Double> elementFromExpectedOutput  = expectedOutput.getFirst();
-    	mustBeEqualTo.addFirst(elementFromExpectedOutput.getValue());
-    	Entry<Double> elementFromMustBeEqualTo  = mustBeEqualTo.getFirst();
-    	elementFromExpectedOutput = elementFromExpectedOutput.getNext();
-    	while(null != elementFromExpectedOutput){
-    		mustBeEqualTo.addAfter(elementFromMustBeEqualTo, elementFromExpectedOutput.getValue());
-    		elementFromMustBeEqualTo = elementFromMustBeEqualTo.getNext();
-    		elementFromExpectedOutput = elementFromExpectedOutput.getNext();
-    	}
-    }
-	
+	private long limit;
+
+	public void doProcess(SortedLimitedList<Double> mustBeEqualTo,	SortedLimitedList<Double> expectedOutput) {
+		if (null != mustBeEqualTo && null != expectedOutput) {
+			mustBeEqualTo.clear();
+			if (expectedOutput.count > 0) {
+				Entry<Double> elementFromExpectedOutput = expectedOutput.getFirst();
+				mustBeEqualTo.addFirst(elementFromExpectedOutput.getValue());
+				Entry<Double> elementFromMustBeEqualTo = mustBeEqualTo.getFirst();
+				elementFromExpectedOutput = elementFromExpectedOutput.getNext();
+				while (null != elementFromExpectedOutput) {
+					mustBeEqualTo.addAfter(elementFromMustBeEqualTo,elementFromExpectedOutput.getValue());
+					elementFromMustBeEqualTo = elementFromMustBeEqualTo.getNext();
+					elementFromExpectedOutput = elementFromExpectedOutput.getNext();
+				}
+			}
+		}
+	}
+
 }

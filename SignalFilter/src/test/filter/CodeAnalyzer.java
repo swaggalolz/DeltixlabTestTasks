78c3a5555387163e82e682c955af7a8e43736870
swaggalolz <rosmen1@tut.by>
2016-05-04 16:04:47 -0300

    update tests

--- SignalFilter/src/test/filter/CodeAnalyzer.java
+++ SignalFilter/src/test/filter/CodeAnalyzer.java
@@ -1,35 +1,90 @@
 package test.filter;
 
+import com.deltixlab.CircularBuffer;
 import com.deltixlab.SignalFilter;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class CodeAnalyzer {
 
+    @State(Scope.Benchmark)  //общий на все потоки
+    public static class BenchmarkFilter {
+        SignalFilter signalFilter = new SignalFilter(3);
+    }
 
-    @State(Scope.Benchmark)
-    public static class BenchmarkState {
+    @State(Scope.Thread)  //экземпл€р дл€ каждого потока
+    public static class ThreadFilter {
         SignalFilter signalFilter = new SignalFilter(3);
     }
+
     @Benchmark
-    public void measureShared(BenchmarkState state) {
-        state.signalFilter.isSignalAllowed();
+    @BenchmarkMode(Mode.AverageTime) //среднее врем€ работы.
+    //@BenchmarkMode(Mode.Throughput)   //число операций в секунду
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void measureForUnsharedFilter( ThreadFilter filter ) {
+        filter.signalFilter.isSignalAllowed();
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    //@BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void measureForSharedFilter( BenchmarkFilter filter) {
+       filter.signalFilter.isSignalAllowed();
+    }
+
+    @State(Scope.Thread)
+    public static class ThreadBuffer {
+        CircularBuffer circularBuffer = new CircularBuffer(3);
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    //@BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void measureForUnsharedBufferAdd( ThreadBuffer buffer, ThreadLongValues values ) {
+        buffer.circularBuffer.add(values.timeNow);
     }
     @Benchmark
-    public void test() {
-        SignalFilter signalFilter = new SignalFilter(3);
-        signalFilter.isSignalAllowed();
-        signalFilter.isSignalAllowed();
-        signalFilter.isSignalAllowed();
-        signalFilter.isSignalAllowed();
-        signalFilter.isSignalAllowed();
+    @BenchmarkMode(Mode.AverageTime)
+    //@BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void measureForUnsharedBufferPeek( ThreadBuffer buffer ) {
+        buffer.circularBuffer.peek();
     }
+
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    //@BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void measureGetTime() {
+        System.currentTimeMillis();
+    }
+
+    @State(Scope.Thread)
+    public static class ThreadLongValues {
+        long timeNow = System.currentTimeMillis();
+        long timeBefore = System.currentTimeMillis();
+        long result;
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    //@BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void measureCalculation( ThreadLongValues values) {
+       values.result = values.timeNow - values.timeBefore;
+    }
+
+
+
 
     public static void main(String ... args) throws RunnerException{
         Options opt = new OptionsBuilder()

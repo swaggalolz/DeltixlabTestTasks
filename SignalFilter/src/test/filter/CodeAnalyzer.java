68916752ddd2e9eb8643767c911e5bb7d47815e1
swaggalolz <rosmen1@tut.by>
2016-05-06 05:02:43 -0300

    added test includes testing of two versions of class

--- SignalFilter/src/test/filter/CodeAnalyzer.java
+++ SignalFilter/src/test/filter/CodeAnalyzer.java
@@ -1,6 +1,7 @@
 package test.filter;
 
 import com.deltixlab.SignalFilter;
+import com.deltixlab.TestFIlter;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
@@ -14,30 +15,54 @@ public class CodeAnalyzer {
 
     @State(Scope.Benchmark)  
     public static class BenchmarkFilter {
-        SignalFilter signalFilter = new SignalFilter(3);
+        SignalFilter signalFilter = new SignalFilter(30);
+        TestFIlter testFIlter = new TestFIlter(30);
     }
 
     @State(Scope.Thread) 
     public static class ThreadFilter {
-        SignalFilter signalFilter = new SignalFilter(3);
+        SignalFilter signalFilter = new SignalFilter(30);
+        TestFIlter testFIlter = new TestFIlter(30);
     }
 
+
     @Benchmark
-    @Warmup(iterations = 500 )
-    @Measurement(iterations = 500)
+    @Warmup(iterations = 500, batchSize = 50)
+    @Measurement(iterations = 500, batchSize = 50)
     @BenchmarkMode(Mode.SingleShotTime)
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    public void measureForUnsharedFilter( ThreadFilter filter ) {
+    public void measureForUnsharedSignalFilter( ThreadFilter filter ) {
         filter.signalFilter.isSignalAllowed();
     }
 
+
     @Benchmark
-    @Warmup(iterations = 500 )
-    @Measurement(iterations = 500)
+    @Warmup(iterations = 500, batchSize = 50)
+    @Measurement(iterations = 500, batchSize = 50)
     @BenchmarkMode(Mode.SingleShotTime)
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    public void measureForSharedFilter( BenchmarkFilter filter) {
+    public void measureForUnsharedTestFilter( ThreadFilter filter ) {
+        filter.testFIlter.isSignalAllowed();
+    }
+
+
+    @Benchmark
+    @Warmup(iterations = 500, batchSize = 50)
+    @Measurement(iterations = 500, batchSize = 50)
+    @BenchmarkMode(Mode.SingleShotTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void measureForSharedSignalFilter( BenchmarkFilter filter) {
        filter.signalFilter.isSignalAllowed();
+    }
+
+
+    @Benchmark
+    @Warmup(iterations = 500, batchSize = 50)
+    @Measurement(iterations = 500, batchSize = 50)
+    @BenchmarkMode(Mode.SingleShotTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void measureForSharedTestFilter( BenchmarkFilter filter) {
+        filter.testFIlter.isSignalAllowed();
     }
 
 
